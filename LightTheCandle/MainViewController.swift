fb785f44768b54023f564f455d889a534dd38de5
Byeonghun Hwang <byeonghwang.com>
2019-04-30 08:56:46 +0900

    Initial Commit

---
+++ LightTheCandle/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewController.swift
+//  LightTheCandle
+//
+//  Created by ktds 10 on 29/04/2019.
+//  Copyright © 2019 kt ds. All rights reserved.
+//
+import UIKit
+import Foundation
+
+class MainViewController : UIViewController{
+    @IBOutlet weak var viewImage:UIImageView!
+    @IBOutlet weak var labelStatus:UILabel!
+    @IBOutlet weak var switchNow:UISwitch!
+    
+    @IBAction func touchSwitch(_ sender:Any){
+        print("switch touched!!")
+    }
+}
